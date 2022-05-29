3319a4afd76e6478cba7f1769c746c541530cf41
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 19:43:06 +0300

    perf: Test - Tests the classes in my models

--- artgallery/models.py
+++ artgallery/models.py
@@ -2,20 +2,39 @@ from unicodedata import category
 from django.db import models
 
 # Create your models here
+
+class Category_Photo(models.Model):
+    category_name = models.CharField(max_length=20)
+
+class Where_Photo(models.Model):
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.TextField()
+    category_photo = models.ForeignKey(Category_Photo, on_delete=models.CASCADE)
+    where_photo =models.ForeignKey(Where_Photo,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
     
+    def save_image(self):
+        self.save()
+    
+    def save_image(self):
+        self.save()
+
+
+
 
     
     # image = models.ImageField(upload_to = 'images/')
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=20)
+
     
-class Where_Photo(models.Model):
-    country = models.CharField(max_length=30)
+
 
 
 
--- artgallery/tests.py
+++ artgallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Category_Photo, Where_Photo, Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.barcelona= Image(image_name = 'Lionel Messi Returns', image_description = 'The best player in the world', category_photo = Category_Photo(category_name = 'football'), where_photo = Where_Photo(country = 'Spain', city = 'Barcelona'))
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.barcelona, Image))
+
