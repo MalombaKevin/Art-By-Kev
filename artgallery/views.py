3234c39faa8cf7c14ff872fd83a3c78b33bc9b2d
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 12:50:41 +0300

    perf: urls.py - connect the view function to specific url

--- artgallery/views.py
+++ artgallery/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import httpResponse
 
 # Create your views here.
-def index(request):
-    return httpResponse("Hello, world. You're at the artgallery index.")
+def artHome(request):
+    return httpResponse("Hello, world. You're at the artgallery .")
