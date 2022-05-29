cba1ffe4f94b4db9c7746e3f34fd33782617daac
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 18:59:37 +0300

    feat: models updated: Adds Location and Category class

--- artgallery/models.py
+++ artgallery/models.py
@@ -1,10 +1,21 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.TextField()
+    
+
+    
     # image = models.ImageField(upload_to = 'images/')
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+    
+class Where_Photo(models.Model):
+    country = models.CharField(max_length=30)
 
 
 
