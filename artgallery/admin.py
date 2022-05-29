80e898acdde6c17425bf97aab401add5602b7817
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 20:46:46 +0300

    feat: Admin Page update- adds models to admin page

--- artgallery/admin.py
+++ artgallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category_Photo, Where_Photo, Image
+
 
 # Register your models here.
+admin.site.register(Category_Photo)
+admin.site.register(Where_Photo)
+admin.site.register(Image)
