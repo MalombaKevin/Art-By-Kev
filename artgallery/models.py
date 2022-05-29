5b0776e2232ac6639393664871a25a5352adff24
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 18:33:11 +0300

    feat: Models - creates model, run checks, make migrations, view migrations then run migrations to reflect on database

--- artgallery/models.py
+++ artgallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    # image = models.ImageField(upload_to = 'images/')
+
+
+
