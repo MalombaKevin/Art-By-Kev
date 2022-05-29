515d23c9852d90a8de8166fc5e90967b31d36f14
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-29 13:13:26 +0300

    Perf:Project Settings-Make the artgallery the landing page on load

--- KevoArt/urls.py
+++ KevoArt/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
-    url('^artgallery/', include('artgallery.urls')),
+    url('', include('artgallery.urls')),
 ]
